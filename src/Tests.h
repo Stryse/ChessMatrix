5d735506449389a97a6edb878e8b54f0531dcf9a
Stryse <tianarath30@gmail.com>
2020-02-29 23:10:09 +0100

    More tests coming...

--- src/Tests.h
+++ src/Tests.h
@@ -4,7 +4,6 @@
 #include <fstream>
 #include "ChessMatrix.h"
 
-// Test for reading a valid input
 TEST_CASE("ReadValidFromFile", "../tests/Test01_READVALID.txt")
 {
     const std::string inputPath = "../tests/Test01_READVALID.txt";
@@ -21,6 +20,8 @@ TEST_CASE("ReadValidFromFile", "../tests/Test01_READVALID.txt")
     ChessMatrix CM(M,N);
     ifs >> CM;
 
+    CHECK(CM.dimM == 3);
+    CHECK(CM.dimN == 3);
     CHECK(CM[0][0] == 1);
     CHECK(CM[0][1] == 0);
     CHECK(CM[0][2] == 2);
@@ -36,9 +37,7 @@ TEST_CASE("ReadValidFromFile", "../tests/Test01_READVALID.txt")
 
 TEST_CASE("ReadInvalidFromFile","../tests/Test02_READINVALID.txt")
 {
-
 }
-
 
 TEST_CASE("Adding two valid matrices","../tests/Test03_ADDVALID.txt")
 {
@@ -62,6 +61,10 @@ TEST_CASE("Adding two valid matrices","../tests/Test03_ADDVALID.txt")
     ifs >> CM2;
 
     ChessMatrix CM3(CM + CM2);
+    CHECK(CM.dimM == CM2.dimM);
+    CHECK(CM.dimN == CM2.dimN);
+    CHECK(CM.dimM == CM3.dimM);
+    CHECK(CM.dimN == CM3.dimN);
     CHECK(CM3[0][0] == 5);
     CHECK(CM3[0][1] == 0);
     CHECK(CM3[0][2] == 5);
@@ -93,15 +96,70 @@ TEST_CASE("Adding two matrices with unmatching dimensions","../tests/Test04_ADDI
     }
 
     size_t M,N;
-
     ifs >> M >> N;
     ChessMatrix CM(M,N);
     ifs >> CM;
 
-    ifs >> M >> N;
-    ChessMatrix CM2(M,N);
+    size_t M2,N2;
+    ifs >> M2 >> N2;
+    ChessMatrix CM2(M2,N2);
     ifs >> CM2;
+
+    CHECK((M != M2 || N != N2));
     CHECK_THROWS(CM + CM2);
+
+    ifs.close();
+}
+
+TEST_CASE("AddTo","../tests/Test08_ADDTO.txt")
+{
+    const std::string inputPath = "../tests/Test08_ADDTO.txt";
+    std::ifstream ifs(inputPath);
+    if(ifs.fail())
+    {
+        std::cout << "Error occured during opening the file: " << inputPath << "! exiting..." << std::endl;
+        std::exit(1);
+    }
+
+    size_t M,N;
+    ifs >> M >> N;
+    ChessMatrix CM(M,N);
+    ifs >> CM;
+
+    size_t M2,N2;
+    ifs >> M2 >> N2;
+    ChessMatrix CANADD(M2,N2);
+    ifs >> CANADD;
+
+    size_t M3,N3;
+    ifs >> M3 >> N3;
+    ChessMatrix CANT_ADD(M3,N3);
+    ifs >> CANT_ADD;
+
+    CM += CANADD;
+
+    SECTION("CAN ADD")
+    {
+        CHECK(CM.dimM == CANADD.dimM);
+        CHECK(CM.dimN == CANADD.dimN);
+        CHECK(CM[0][0] == 10);
+        CHECK(CM[0][1] == 0);
+        CHECK(CM[0][2] == 10);
+        CHECK(CM[1][0] == 0);
+        CHECK(CM[1][1] == 10);
+        CHECK(CM[1][2] == 0);
+        CHECK(CM[2][0] == 10);
+        CHECK(CM[2][1] == 0);
+        CHECK(CM[2][2] == 10);
+    }
+
+    SECTION("CANT ADD (unmatching dimensions)")
+    {
+        CHECK((CM.dimM != CANT_ADD.dimM || CM.dimN != CANT_ADD.dimN));
+        CHECK_THROWS(CM += CANT_ADD);
+    }
+
+    ifs.close();
 }
 
 TEST_CASE("GetElement","../test/Test05_GETELEMENT.txt")
@@ -153,6 +211,79 @@ TEST_CASE("GetElement","../test/Test05_GETELEMENT.txt")
         CHECK_THROWS(CM[0][CM.dimN]);
         CHECK_THROWS(CM[CM.dimM][CM.dimN]);
     }
-    
+
+    ifs.close();
 }
+
+TEST_CASE("Copy Constructor","../tests/Test06_COPYCTOR.txt")
+{
+    const std::string inputPath = "../tests/Test06_COPYCTOR.txt";
+    std::ifstream ifs(inputPath);
+    if(ifs.fail())
+    {
+        std::cout << "Error occured during opening the file: " << inputPath << "! exiting..." << std::endl;
+        std::exit(1);
+    }
+    size_t M,N;
+    ifs >> M >> N;
+    ChessMatrix CM(M,N);
+    ifs >> CM;
+
+    ChessMatrix COPIED = CM;
+
+    CHECK(CM.dimM == COPIED.dimM);
+    CHECK(CM.dimN == COPIED.dimN);
+    CHECK(CM[0][0] == COPIED[0][0]);
+    CHECK(CM[0][1] == COPIED[0][1]);
+    CHECK(CM[0][2] == COPIED[0][2]);
+    CHECK(CM[1][0] == COPIED[1][0]);
+    CHECK(CM[1][1] == COPIED[1][1]);
+    CHECK(CM[1][2] == COPIED[1][2]);
+    CHECK(CM[2][0] == COPIED[2][0]);
+    CHECK(CM[2][1] == COPIED[2][1]);
+    CHECK(CM[2][2] == COPIED[2][2]);
+
+    ifs.close();
+}
+
+TEST_CASE("Copy Assignment operator","../tests/Test07_COPYASSIGNMENT.txt")
+{
+    const std::string inputPath = "../tests/Test07_COPYASSIGNMENT.txt";
+    std::ifstream ifs(inputPath);
+    if(ifs.fail())
+    {
+        std::cout << "Error occured during opening the file: " << inputPath << "! exiting..." << std::endl;
+        std::exit(1);
+    }
+    size_t M,N;
+    ifs >> M >> N;
+    ChessMatrix CM(M,N);
+    ifs >> CM;
+    ChessMatrix COPIED(M,N);
+    ChessMatrix CANT_COPY(M+1,N+1);
+
+    COPIED = CM;
+    SECTION("Can Copy (matching dimensions)")
+    {
+        CHECK(CM.dimM == COPIED.dimM);
+        CHECK(CM.dimN == COPIED.dimN);
+        CHECK(CM[0][0] == COPIED[0][0]);
+        CHECK(CM[0][1] == COPIED[0][1]);
+        CHECK(CM[0][2] == COPIED[0][2]);
+        CHECK(CM[1][0] == COPIED[1][0]);
+        CHECK(CM[1][1] == COPIED[1][1]);
+        CHECK(CM[1][2] == COPIED[1][2]);
+        CHECK(CM[2][0] == COPIED[2][0]);
+        CHECK(CM[2][1] == COPIED[2][1]);
+        CHECK(CM[2][2] == COPIED[2][2]);
+    }
+    SECTION("Can't copy (unmatching dimensions)")
+    {
+        CHECK((CM.dimM != CANT_COPY.dimM || CM.dimN != CANT_COPY.dimN));
+        CHECK_THROWS(CANT_COPY = CM);
+    }
+
+    ifs.close();
+}
+
 #endif
